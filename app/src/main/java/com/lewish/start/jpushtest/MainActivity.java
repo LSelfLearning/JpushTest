6ae540fbb14370ec8a6b8db380b0afd5105aefe3
sundong <sundong@yingu.com>
2016-09-27 18:05:06 +0800

    Initial commit

---
+++ app/src/main/java/com/lewish/start/jpushtest/MainActivity.java
@@ -0,0 +1,28 @@
+package com.lewish.start.jpushtest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import cn.jpush.android.api.JPushInterface;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        JPushInterface.onResume(this);
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        JPushInterface.onPause(this);
+        super.onPause();
+    }
+}
